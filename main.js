5114280c0eb0e2c9c2f979c93aa1c53323210bd9
anonymous-zaks <anonymouszaks@gmail.com>
2025-08-28 16:50:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+// alert("HI, JAVASCRIPT IS WORKING");
+var tablinks=document.getElementsByClassName("tab-links");
+    var tabcontents=document.getElementsByClassName("tab-contents");
+    function opentab(tabname){
+        for(tablink of tablinks){
+            tablink.classList.remove("active-link");
+        }
+        for(tabcontent of tabcontents){
+            tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+    }
+    var sidemenu=document.getElementById("sidemenu");
+    function openmenu(){
+        sidemenu.style.right="0";
+    }
+    function closemenu(){
+        sidemenu.style.right="-200px";
+    }
+    const scriptURL =' https://script.google.com/macros/s/AKfycbxUg2BKXWxab9xLmRRmw9WqNQXE3EvjvCG1bjANPckGILbiSmtkY0P8y8q5oBTSUTAx0Q/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg= document.getElementById("msg");
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML="Message sent successfully!";
+        setTimeout(function(){
+            msg.innerHTML="";
+        },5000);
+        form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
